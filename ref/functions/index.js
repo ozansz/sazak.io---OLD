b84ee62012aaa6f3dc6895489e3347ec83225bd9
Ozan Sazak <ozan.sazak@protonmail.ch>
2020-02-08 14:30:48 +0300

    added utils test

--- ref/functions/index.js
+++ ref/functions/index.js
@@ -1,8 +1,9 @@
 const functions = require('firebase-functions');
+const utils = require('./utils')
 
 // Create and Deploy Your First Cloud Functions
 // https://firebase.google.com/docs/functions/write-firebase-functions
 
 exports.helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+ response.send(utils.getRandomID());
 });
