46841f982417376748950dc8df2fda885d6e6435
Ozan Sazak <ozan.sazak@protonmail.ch>
2020-02-08 22:20:11 +0300

    added header and footer

--- ref-ui/src/App.vue
+++ ref-ui/src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
+    <a href="https://sazak.io" class="app__home-link">Homepage</a>
     <router-view/>
+    <div class="app__desc">Developed by Ozan Sazak. This project is open source. Please refer to <a target="_blank" href="https://github.com/ozansz/sazak.io">Github</a></div>
   </div>
 </template>
 
@@ -18,5 +20,21 @@ html, body {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   width: 100vw;
   height: 100vh;
+}
+.app__home-link {
+  font-size: 1.2em;
+  color: #fff;
+  position: absolute;
+  top: 32px;
+  left: 48px;
+}
+.app__desc {
+  color: #fff;
+  font-size: 1em;
+  position: absolute;
+  bottom: 15px;
+  left: 0;
+  right: 0;
+  text-align: center;
 }
 </style>
